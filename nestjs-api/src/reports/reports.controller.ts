65b8fea0204ed35639ddd0059b1426b55c456c7e
lincon <linconsilva08@gmail.com>
2021-09-19 12:33:20 -0300

    Class 3 - concluded

--- nestjs-api/src/reports/reports.controller.ts
+++ nestjs-api/src/reports/reports.controller.ts
@@ -1,21 +1,30 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
+import { MessagePattern, Payload } from '@nestjs/microservices'
+import { KafkaMessage } from '@nestjs/microservices/external/kafka.interface'
 import { JwtAuthGuard } from 'src/auth/jwt-auth-guard'
 import { TenantGuard } from 'src/tenant/tenant.guard'
 import { CreateReportDto } from './dto/create-report.dto'
 import { ReportsService } from './reports.service'
 
-@UseGuards(JwtAuthGuard, TenantGuard)
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
+  @UseGuards(JwtAuthGuard, TenantGuard)
   @Post()
   create(@Body() createReportDto: CreateReportDto) {
     return this.reportsService.create(createReportDto)
   }
 
+  @UseGuards(JwtAuthGuard, TenantGuard)
   @Get()
   findAll() {
     return this.reportsService.findAll()
+  }
+
+  @MessagePattern('reports-generated')
+  async reportGenerated(@Payload() message: KafkaMessage) {
+    const { id, ...others } = message.value as any
+    await this.reportsService.update(id, others)
   }
 }
